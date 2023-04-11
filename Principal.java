749644a20ef0519cabe6c8f65de536e258f24d3e
brazyon <117491562+brazyon@users.noreply.github.com>
2023-04-11 20:57:15 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,13 @@
+package Principal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            ClienteSocket cliente = new ClienteSocket();
+            cliente.comunicarComServidor();
+        } catch (Exception e) {
+            System.out.println("Exceção:" + e);
+        }
+    }
+}
